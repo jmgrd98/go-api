bc9d56c40e1d48b2b33963b7eb5fbd689d868e7a
jmgrd98 <jmgrd98@gmail.com>
2025-04-29 18:06:06 -0300

    setting up air & get and post routes

--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
-import "fmt"
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	app.Post("api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	app.Listen(":4000")
+
+	log.Fatal((app.Listen(":4000")))
 }
